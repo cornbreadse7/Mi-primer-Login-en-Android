acd728c0a8d3cfe2122256067e4e31048f37443a
cornbreadse7 <80598656+cornbreadse7@users.noreply.github.com>
2023-03-06 12:21:48 -0600

    Cambios en el UI

---
+++ app/src/main/java/com/ejemplo/layouts/ralative.kt
@@ -0,0 +1,11 @@
+package com.ejemplo.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ralative : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ralative)
+    }
+}
